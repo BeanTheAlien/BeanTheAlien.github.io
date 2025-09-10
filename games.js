d47495c42f789a24734c2ea99daa85c4d71e2831
BeanTheAlien <ben.allen.goldstein@gmail.com>
2025-09-10 10:50:25 -0500

    Started flappy bird implementation

--- games.js
+++ games.js
@@ -17,10 +17,74 @@ const buttons = [{
     "filename": "flappybird",
     "name": "Flappy Bird",
     "win": `
+        class Player {
+            constructor() {
+                this.height = Math.round(c.height / 2);
+                this.grav = 0.05;
+                this.gspd = 0;
+                this.x = 15;
+                this.w = 20;
+                this.h = 20;
+                this.colour = "yellow";
+            }
+            upd() {
+                this.gspd += this.grav;
+                this.height += this.gspd;
+            }
+        }
+        class Pipe {
+            constructor(x, pt, pb) {
+                this.x = x;
+                this.pt = pt;
+                this.pb = pb;
+                this.w = 50;
+                this.colour = "green";
+            }
+        }
+
         const c = document.createElement("canvas");
         c.style.border = "2px solid black";
+        c.width = 800;
+        c.height = 600;
         document.body.appendChild(c);
-        // The rest of the flappy bird game code would go here
+
+        var player = new Player();
+        var pipes = [];
+        var delta = 0;
+        var deltasr = 500;
+        var gap = 150;
+        const ctx = c.getContext("2d");
+
+        if((delta % deltaSpawn) == 0) {
+            let topHeight = Math.floor(Math.random() * (c.height - gap - 50));
+            let bottomYt = topHeight + gap;
+            pipes.push(new Pipe(c.width, 0, topHeight));
+            pipes.push(new Pipe(c.width, bottomYt, c.height));
+        }
+    ctx.clearRect(0, 0, gameboard.width, gameboard.height);
+
+        export class BirdPlayer {
+    constructor(canvas) {
+        this.height = Math.round(canvas.height / 2);
+        this.gravity = 0.05;
+        this.gravitySpeed = 0;
+    }
+    update(Runtime, gravRuntime) {
+        this.gravitySpeed += this.gravity;
+        this.height += this.gravitySpeed;
+        // if(this.height <= 600) {
+        //     clearInterval(Runtime);
+        //     clearInterval(gravRuntime);
+        // }
+    }
+}
+export class BirdPipe {
+    constructor(x, yTop, yBottom) {
+        this.pX = x;
+        this.pYt = yTop;
+        this.pYb = yBottom;
+    }
+}
     `
 }];
 
