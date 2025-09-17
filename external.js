16e04b4b239098bd3ac5b82d6d29a88b718b7a24
BeanTheAlien <ben.allen.goldstein@gmail.com>
2025-09-17 17:09:06 -0500

    Fixed an oversight in external.js

--- external.js
+++ external.js
@@ -1,7 +1,6 @@
-window.addEventListener("error", (e) => alert(e));
-alert();
 const querys = new URLSearchParams(window.location.search);
 const pages = {
   "glarrglan": "https://docs.google.com/document/d/19gape2_N_B5AwshPbWARyjTViMeV_pr5pLitBynVlYg/edit?usp=sharing"
 };
-if(pages[querys.get("id")) window.location.href = pages[querys.get("id")];
+const pg = querys.get("id");
+if(pages[pg]) window.location.href = pages[pg];
