60ee9b97ab0d935491ef4bab65152457c1416137
BeanTheAlien <ben.allen.goldstein@gmail.com>
2025-09-17 11:43:35 -0500

    Update external.js

--- external.js
+++ external.js
@@ -1,5 +1,6 @@
+window.addEventListener("error", (e) => alert(e));
 const querys = new URLSearchParams(window.location.search);
 const pages = {
   "glarrglan": "https://docs.google.com/document/d/19gape2_N_B5AwshPbWARyjTViMeV_pr5pLitBynVlYg/edit?usp=sharing"
 };
-window.location.href = pages[querys.get("id")];
+if(pages[querys.get("id")) window.location.href = pages[querys.get("id")];
