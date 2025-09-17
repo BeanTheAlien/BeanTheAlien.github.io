925507dd40d7c9fdf88e162f2ff7669095633dd0
BeanTheAlien <ben.allen.goldstein@gmail.com>
2025-09-17 11:39:16 -0500

    missed a closing bracket

--- external.js
+++ external.js
@@ -3,5 +3,5 @@ const pages = {
   "glarrglan": "https://docs.google.com/document/d/19gape2_N_B5AwshPbWARyjTViMeV_pr5pLitBynVlYg/edit?usp=sharing"
 };
 try {
-  window.open(pages[querys.get("id"), "_blank");
-} catch(e) { alert(e); console.error("An error occured: " + e); }
+  window.open(pages[querys.get("id")], "_blank");
+} catch(e) { console.error("An error occured: " + e); }
