50f18cf14a665295f69891c6ae436fd8f3791f05
BeanTheAlien <ben.allen.goldstein@gmail.com>
2025-09-17 11:30:15 -0500

    Created external and added a mapping

---
+++ external.js
@@ -0,0 +1,7 @@
+const querys = new URLSearchParams(window.location.search);
+const pages = {
+  "glarrglan": "https://docs.google.com/document/d/19gape2_N_B5AwshPbWARyjTViMeV_pr5pLitBynVlYg/edit?usp=sharing"
+};
+try {
+  window.open(pages[querys.get("id"), "_blank");
+} catch(e) { console.error("An error occured: " + e); }
