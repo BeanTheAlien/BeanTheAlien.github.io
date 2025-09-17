e9f18a4cdcf47334ffb9df7603a64021ca5d1dd2
BeanTheAlien <ben.allen.goldstein@gmail.com>
2025-09-17 11:00:18 -0500

    Fixed a minor oversight in Among Us

--- games.js
+++ games.js
@@ -188,7 +188,7 @@ const games = [
                     const cmates = lobby.filter(l => l.role == "crewmate");
                     const clCmate = [...cmates].sort((a, b) => dist(this.x, this.y, a.x, a.y) - dist(this.x, this.y, b.x, b.y))[0];
                     const clTask = [...this.tasks].sort((a, b) => dist(this.x, this.y, a.x, a.y) - dist(this.x, this.y, b.x, b.y))[0];
-                    if(clMate <= clTask && this.killtimer == 0) {} // will path to them & kill them
+                    if(clCmate <= clTask && this.killtimer == 0) {} // will path to them & kill them
                     else {} // path to task & pretend to do
                 }
             }
