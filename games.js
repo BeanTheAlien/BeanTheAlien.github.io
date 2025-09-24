5468ab3fa87075ebf4a770b24e0567e1dc3473a2
BeanTheAlien <ben.allen.goldstein@gmail.com>
2025-09-24 08:47:36 -0500

    increased grav strength AGAIN
    @FRLandek please stop complaining i fixed it

--- games.js
+++ games.js
@@ -59,7 +59,7 @@ const games = [
             class Player {
                 constructor() {
                     this.height = Math.round(c.height / 2);
-                    this.grav = 0.07;
+                    this.grav = 0.077;
                     this.gspd = 0;
                     this.x = 15;
                     this.y = 30;
