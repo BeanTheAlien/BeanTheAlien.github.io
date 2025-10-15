9b16d07a87db9e4842f21bfc90a8d7261ddca926
BeanTheAlien <ben.allen.goldstein@gmail.com>
2025-10-15 13:34:37 -0500

    Started adding ghost.io

---
+++ ghost/modules/ghost/io.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const path = require("path");
+
+const module = {
+    name: "io",
+    desc: "The I/O module for GhostScript.",
+    bundle: "ghost",
+    reqroot: false,
+    defroot: "io"
+};
+
+function writeFile(filepath, content) {
+    fs.writeFile(path.join(__dirname, filepath), content);
+}
+
+function readFile(filepath) {
+    return fs.readFile(path.join(__dirname, filepath));
+}
+
+function appendFile(filepath, content) {
+    fs.appendFile(path.join(__dirname, filepath), content);
+}
