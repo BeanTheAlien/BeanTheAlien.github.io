d8c268809de4a6623a6be8b1f68a74e2fd703858
BeanTheAlien <ben.allen.goldstein@gmail.com>
2025-10-16 07:44:35 -0500

    Added random & format

---
+++ ghost/modules/ghost/random.js
@@ -0,0 +1,41 @@
+const ghostmodule = {
+    name: "random",
+    desc: "The Random module for GhostScript.",
+    version: "1.0.0",
+    author: "GhostScript",
+    root: "ghost",
+    reqroot: false,
+    defroot: "random"
+};
+
+function random(a = null, b = null) {
+    if(a == null && b == null) {
+        return Math.floor(Math.random() * 101);
+    } else if(a != null && b == null) {
+        return Math.floor(Math.random() * a);
+    } else if(a != null && b != null) {
+        let min;
+        let max;
+        if(a > b) {
+            min = Math.ceil(b);
+            max = Math.floor(a);
+            return Math.floor(Math.random() * (max - min)) + min;
+        } else if(a < b) {
+            min = Math.ceil(a);
+            max = Math.floor(b);
+            return Math.floor(Math.random() * (max - min)) + min;
+        } else {
+            return Math.floor(Math.random() * a);
+        }
+    }
+}
+
+function chance(floor = 50) {
+    return (random() <= floor);
+}
+
+module.exports = {
+    random,
+    chance,
+    ghostmodule
+};
