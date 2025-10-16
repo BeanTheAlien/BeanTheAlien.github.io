d0670e967bd41786d632da0dd34157803440e9cb
BeanTheAlien <ben.allen.goldstein@gmail.com>
2025-10-16 08:26:48 -0500

    Worked on stdlib

---
+++ ghost/modules/ghost/ghost.js
@@ -0,0 +1,51 @@
+const ghostmodule = {
+    name: "ghost",
+    desc: "The GhostScript standard library.",
+    version: "1.0.0",
+    author: "GhostScript",
+    root: "ghost",
+    reqroot: false,
+    defroot: "ghost"
+};
+
+class GSVar {
+    constructor(s) {
+        this.gsVarMods = s.gsVarMods;
+        this.gsVarType = s.gsVarType;
+        this.gsVarName = s.gsVarName;
+        this.gsVarVal = s.gsVarVal;
+    }
+}
+class GSFunc {}
+class GSMethod {}
+class GSClass {}
+class GSType {}
+class GSProp {}
+class GSErr extends Error {
+    constructor(nm, msg) {
+        super(`${nm}: ${msg}`);
+    }
+}
+class InternalJavaScriptError extends GSErr {
+    constructor() {
+        super("InternalJavaScriptError", "An internal JS error occured.");
+    }
+}
+class ImportMissingError extends GSErr {
+    constructor(imp) {
+        super("ImportMissingError", `Import '${imp}' does not exist.`)
+    }
+}
+
+module.exports = {
+    GSVar,
+    GSFunc,
+    GSMethod,
+    GSClass,
+    GSType,
+    GSProp,
+    GSErr,
+    InternalJavaScriptError,
+    ImportMissingError,
+    ghostmodule
+};
