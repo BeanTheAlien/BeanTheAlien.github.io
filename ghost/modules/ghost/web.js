09cea5c1d1e704ab51aa7d5e78c849b14b4379ab
BeanTheAlien <ben.allen.goldstein@gmail.com>
2025-10-20 07:38:28 -0500

    Added ghost.web module

---
+++ ghost/modules/ghost/web.js
@@ -0,0 +1,52 @@
+const fs = require("fs");
+const cp = require("child_process");
+
+const ghostmodule = {
+    name: "web",
+    desc: "The Web module for GhostScript.",
+    version: "1.0.0",
+    author: "GhostScript",
+    root: "ghost",
+    reqroot: true,
+    defroot: "web"
+};
+
+class WebDoc {
+    constructor(content = []) {
+        if(!Array.isArray(content)) throw new Error("Content must be an array!");
+        this.content = content;
+    }
+    add(...stuff) {
+        this.content.push(...stuff);
+    }
+    rem(...stuff) {
+        for(let i = this.content.length - 1; i >= 0; i--) {
+            if(stuff.includes(this.content[i])) {
+                this.content.splice(i, 1);
+            }
+        }
+    }
+    set(c) {
+        this.content = c;
+    }
+    get() {
+        return this.content;
+    }
+    save() {
+        fs.writeFile("temp.html", this.content.join(""));
+    }
+    launchWin() {
+        cp.exec(`start "" "${__dirname}/temp.html"`);
+    }
+    launchMac() {
+        cp.exec(`open "${__dirname}/temp.html"`);
+    }
+    launchLinux() {
+        cp.exec(`xdg-open "${__dirname}/temp.html"`);
+    }
+}
+
+module.exports = {
+    WebDoc,
+    ghostmodule
+};
