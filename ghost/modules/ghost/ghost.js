91a3cc365e0985c8a3e5e09477313bd61d84bf1d
BeanTheAlien <ben.allen.goldstein@gmail.com>
2025-11-04 08:07:54 -0600

    Fixed an error where the module dev api couldnt be found

--- ghost/modules/ghost/ghost.js
+++ ghost/modules/ghost/ghost.js
@@ -1,5 +1,5 @@
 const readline = require("readline");
-const { GSVar, GSFunc, GSMethod, GSClass, GSType, GSProp, GSModifier, GSErr, GSEvent, GSGroup, GSOperator, GSDirective, GSArg, GSManager } = require("../../dev/module_dev");
+const { GSVar, GSFunc, GSMethod, GSClass, GSType, GSProp, GSModifier, GSErr, GSEvent, GSGroup, GSOperator, GSDirective, GSArg, GSManager } = require("ghost/dev/module_dev.js");
 
 const ghostmodule = {
     name: "ghost",
