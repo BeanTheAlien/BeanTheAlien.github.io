003b558138ca93d936f6b122db60b8dd1a2fde90
BeanTheAlien <ben.allen.goldstein@gmail.com>
2025-11-17 07:44:48 -0600

    Updated random

--- ghost/modules/ghost/random.js
+++ ghost/modules/ghost/random.js
@@ -10,31 +10,58 @@ const ghostmodule = {
     defroot: "random"
 };
 
-function random(a = null, b = null) {
-    if(a == null && b == null) {
-        return Math.floor(Math.random() * 101);
-    } else if(a != null && b == null) {
-        return Math.floor(Math.random() * a);
-    } else if(a != null && b != null) {
-        let min;
-        let max;
-        if(a > b) {
-            min = Math.ceil(b);
-            max = Math.floor(a);
-            return Math.floor(Math.random() * (max - min)) + min;
-        } else if(a < b) {
-            min = Math.ceil(a);
-            max = Math.floor(b);
-            return Math.floor(Math.random() * (max - min)) + min;
-        } else {
+const random = new GSFunc({
+    gsFuncDesire: false,
+    gsFuncType: "int",
+    gsFuncName: "random",
+    gsFuncArgs: [
+        new GSArg({ gsArgName: "a", gsArgVal: null, gsArgDesire: true, gsArgType: "int" }),
+        new GSArg({ gsArgName: "b", gsArgVal: null, gsArgDesire: true, gsArgType: "int" })
+    ],
+    gsFuncBody: (a, b) => {
+        if(a == null && b == null) {
+            return Math.floor(Math.random() * 101);
+        } else if(a != null && b == null) {
             return Math.floor(Math.random() * a);
+        } else if(a != null && b != null) {
+            let min;
+            let max;
+            if(a > b) {
+                min = Math.ceil(b);
+                max = Math.floor(a);
+                return Math.floor(Math.random() * (max - min)) + min;
+            } else if(a < b) {
+                min = Math.ceil(a);
+                max = Math.floor(b);
+                return Math.floor(Math.random() * (max - min)) + min;
+            } else {
+                return Math.floor(Math.random() * a);
+            }
         }
     }
-}
-
-function chance(floor = 50) {
-    return (random() <= floor);
-}
+});
+const chance = new GSFunc({
+    gsFuncDesire: false,
+    gsFuncType: "bool",
+    gsFuncName: "chance",
+    gsFuncArgs: [new GSArg({ gsArgName: "floor", gsArgVal: null, gsArgDesire: true, gsArgType: "int" })],
+    gsFuncBody: (floor) => {
+        return random.gsFuncBody() <= floor;
+    }
+});
+const ranBool = new GSFunc({
+    gsFuncDesire: false,
+    gsFuncType: "bool",
+    gsFuncName: "ranBool",
+    gsFuncArgs: [],
+    gsFuncBody: () => {
+        return chance.gsFuncBody(50);
+    }
+});
+// this.gsArgName = s.gsArgName;
+// this.gsArgVal = s.gsArgVal;
+// this.gsArgDesire  = s.gsArgDesire;
+// this.gsArgType = s.gsArgType;
 
 module.exports = {
     random,
