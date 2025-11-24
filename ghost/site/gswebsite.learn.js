e5ec35cc0932bfb8b0b7ac5d37ea0ec8ab7374f5
BeanTheAlien <ben.allen.goldstein@gmail.com>
2025-11-24 11:23:30 -0600

    Refactor lessons structure and update chapters population

--- ghost/site/gswebsite.learn.js
+++ ghost/site/gswebsite.learn.js
@@ -1,6 +1,18 @@
 import { mk, add, rem, el, style } from "./htmlutils.js";
 window.location.href = "gswebsite.construction.html";
 const title = mk("h1", { textContent: "Learn GhostScript" });
-const lessons = {};
+const lessonsOpts = [
+  { section: "test", items: [] }
+];
+const lessons = {
+  "1.1": {
+    title: "",
+    body: ""
+  }
+};
 const chapters = mk("select");
-for(const { key, value } of Object.entries(lessons)) add(mk("option", { textContent: null, value: null }), chapters);
+for(const { section, items } of lessonsOpts) {
+  const optgroup = mk("optgroup");
+  for(const { title, val } of items) add(mk("option", { textContent: title, value: val }), optgroup);
+  add(optgroup, chapters);
+}
