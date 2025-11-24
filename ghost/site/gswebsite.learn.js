6539d370dc382cf5336649070d38d4f45c0dad33
BeanTheAlien <ben.allen.goldstein@gmail.com>
2025-11-24 09:33:17 -0600

    Fixed artifacts that refd wrong js script

--- ghost/site/gswebsite.learn.js
+++ ghost/site/gswebsite.learn.js
@@ -1,1 +1,6 @@
+import { mk, add, rem, el, style } from "./htmlutils.js";
 window.location.href = "gswebsite.construction.html";
+const title = mk("h1", { textContent: "Learn GhostScript" });
+const lessons = {};
+const chapters = mk("select");
+for(const { key, value } of Object.entries(lessons)) add(mk("option", { textContent: null, value: null }), chapters);
