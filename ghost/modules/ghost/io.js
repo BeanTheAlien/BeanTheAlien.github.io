08bced54f64a0f5f19e83b9f3f864ae5ca414c6b
BeanTheAlien <ben.allen.goldstein@gmail.com>
2026-01-14 08:44:15 -0600

    Fixed an `undefined` err

--- ghost/modules/ghost/io.js
+++ ghost/modules/ghost/io.js
@@ -77,12 +77,12 @@ const cp = new GSFunc({
 const json = {
     read: new GSFunc({
         gsFuncDesire: false, gsFuncType: string,
-        gsFuncName: "read", gsFuncArgs: [new GSArg({ gsArgName: "filepath", gsArgVal: null, gsArgDesire: true, gsArgType: string })],
+        gsFuncName: "json.read", gsFuncArgs: [new GSArg({ gsArgName: "filepath", gsArgVal: null, gsArgDesire: true, gsArgType: string })],
         gsFuncBody: (filepath) => JSON.parse(fs.readFileSync(filepath))
     }),
     write: new GSFunc({
         gsFuncDesire: false, gsFuncType: gsVoid,
-        gsFuncName: "write", gsFuncArgs: [
+        gsFuncName: "json.write", gsFuncArgs: [
             new GSArg({ gsArgName: "filepath", gsArgVla: null, gsArgDesire: true, gsArgType: string }),
             new GSArg({ gsArgName: "content", gsArgVal: null, gsArgDesire: true, gsArgType: entity })
         ],
@@ -94,6 +94,6 @@ module.exports = {
     read, write,
     readdir, append,
     cp, rem, remdir,
-    exists, mkdir, json,
+    exists, mkdir, ...json,
     ghostmodule
 };
