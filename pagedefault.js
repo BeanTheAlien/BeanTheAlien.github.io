e4f9dd4281b62ae6a368407dd281e00723874eed
BeanTheAlien <ben.allen.goldstein@gmail.com>
2026-02-24 09:03:28 -0600

    Fixed page hyperlinks

--- pagedefault.js
+++ pagedefault.js
@@ -13,8 +13,8 @@ const add = (els) => {
 
 const topbarContainer = document.createElement("div");
 topbarContainer.className = "container";
-const buttonHome = mk("Home", "index.html");
-const buttonGames = mk("Games", "games.html");
+const buttonHome = mk("Home", "../../");
+const buttonGames = mk("Games", "../../games/");
 const buttonProjects = mk("Projects", "projects.html");
 const buttonResources = mk("Resources", "resources.html");
 add([buttonHome, buttonGames, buttonProjects, buttonResources]);
