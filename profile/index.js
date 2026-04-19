2dcaf5974ae3f8d2cc2c6dff7f1e2cf99180fd77
BeanTheAlien <ben.allen.goldstein@gmail.com>
2026-04-18 21:31:17 -0500

    updatre profile page handling

--- profile/index.js
+++ profile/index.js
@@ -1,1 +1,8 @@
 import { api } from "../api.js";
+
+const user = await api.sendPost("user");
+const username = document.getElementById("username");
+const pfp = document.getElementById("pfp");
+username.innerText = (await user.json()).u?.username;
+const pfpHex = (await (await api.sendPost("getpfp")).json()).pfp;
+if(pfpHex) { const hex = Uint8Array.fromHex(pfpHex); if(hex) pfp.src = (new TextDecoder()).decode(hex); }
