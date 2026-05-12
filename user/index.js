17985ddd79cc841d634c14cb5cb6a46996959260
BeanTheAlien <ben.allen.goldstein@gmail.com>
2026-05-12 07:31:35 -0500

    fix fetch url

--- user/index.js
+++ user/index.js
@@ -1,4 +1,4 @@
 const uuid = (new URLSearchParams(window.location.search)).get("uuid");
-const res = await fetch(`/meta/${uuid}`);
+const res = await fetch(`https://beanthealien-server.onrender.com/meta/${uuid}`);
 const u = await res.json();
 document.getElementById("u").textContent = u.username;
