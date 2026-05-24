939afa705d39c35993cf8923e58b6fc98b2873e6
BeanTheAlien <ben.allen.goldstein@gmail.com>
2026-05-23 21:15:31 -0500

    change stuff

--- community/createpost/index.js
+++ community/createpost/index.js
@@ -3,9 +3,8 @@ import { comNet } from "../../sv.js";
 const form = document.getElementById("form");
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    const data = new FormData(form);
     comNet.json("post", {
-        title: data.get("title").valueOf(),
-        body: data.get("body").valueOf()
+        title: document.getElementById("title").value,
+        body: document.getElementById("body").value
     });
 });
