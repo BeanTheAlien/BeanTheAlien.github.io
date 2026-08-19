f1bcd28be02d8332f4a7971e106db259f708213f
BeanTheAlien <ben.allen.goldstein@gmail.com>
2026-08-18 19:44:51 -0500

    update dngrm

--- games/dngrm/index.js
+++ games/dngrm/index.js
@@ -5,32 +5,28 @@ var stat = {
     dmg: 1
 };
 const plr = new PlayableCharacter({ strength: 0, width: size, height: size, color: "#29ad05", upd: () => {
-    plr.rot = scene.rotToMouse(plr);
-} });
+        plr.rot = scene.rotToMouse(plr);
+    } });
 plr.binds(["w", () => plr.moveY(-size)], ["a", () => plr.moveX(-size)], ["s", () => plr.moveY(size)], ["d", () => plr.moveX(size)]);
 var pos = new Vector(0, 0);
-interface Room {
-    at: Vector;
-    e: Enemy[];
-}
 const rooms = [];
 class Enemy extends Entity {
-    dmg: number;
-    constructor(x: number, y: number, w: number, h: number, c: string, hp: number, dmg: number) {
+    dmg;
+    constructor(x, y, w, h, c, hp, dmg) {
         super({ x, y, width: w, height: h, color: c });
         this.use("health", { hp, onHurt: () => {
-            this.color = "#a91515";
-            setTimeout(() => this.color = c, 125);
-        }, onDie: () => scene.rm(this) });
+                this.color = "#a91515";
+                setTimeout(() => this.color = c, 125);
+            }, onDie: () => scene.rm(this) });
         scene.add(this);
         this.dmg = dmg;
     }
 }
-
 scene.add(plr);
 scene.on("click", () => {
     const o = (new DebugRay({ origin: plr.getPos(), angle: plr.rot, dist: 5, scene, color: "#e2e603", life: 1.5 })).cast()?.obj;
-    if(o && objIs(o, Enemy)) o.comp("health").hurt(stat.dmg);
+    if (o && objIs(o, Enemy))
+        o.comp("health").hurt(stat.dmg);
 });
 scene.start(() => {
     scene.bg("#003764");
