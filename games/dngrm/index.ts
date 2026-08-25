cfe1b8d331109bd2815ccb66dd890f1092e52285
BeanTheAlien <ben.allen.goldstein@gmail.com>
2026-08-25 07:55:35 -0500

    major changes for dngrm

--- games/dngrm/index.ts
+++ games/dngrm/index.ts
@@ -4,11 +4,16 @@ const size = 10;
 var stat = {
     dmg: 1,
     spd: 3,
-    bspd: 4
+    bspd: 4,
+    hp: 5
 };
 const plr = new PlayableCharacter({ strength: 0, width: size, height: size, color: "#29ad05", upd: () => {
     plr.rot = scene.rotToMouse(plr);
 } });
+plr.use("health", { hp: stat.hp, onHurt: () => {
+    plr.color = "#8b0b0b";
+    setTimeout(() => plr.color = "#29ad05", 125);
+}, onDie: scene.stop });
 plr.binds(["w", () => plr.moveY(-stat.spd)], ["a", () => plr.moveX(-stat.spd)], ["s", () => plr.moveY(stat.spd)], ["d", () => plr.moveX(stat.spd)]);
 var pos = new Vector(0, 0);
 interface Room {
@@ -17,28 +22,65 @@ interface Room {
     exit: Exit[];
 }
 class Enemy extends Entity {
-    dmg: number;
-    constructor(x: number, y: number, w: number, h: number, c: string, hp: number, dmg: number) {
+    atkCd: Cooldown;
+    constructor(x: number, y: number, w: number, h: number, c: string, hp: number, dmg: number, atk: Function, cd: number) {
         super({ x, y, width: size * w, height: size * h, color: c, upd: () => {
-            const r = new DebugRay({ scene, dist: 200, angle: scene.rotBtwn(this, plr), origin: this.getPos(), color: "#ff3c00" });
-            setTimeout(() => scene.rmMisc(r), 500);
-            const h = r.cast();
-            if(!h || h.obj == this) return;
-            this.x += Math.sign(plr.x - this.x);
-            this.y += Math.sign(plr.y - this.y);
+            if(this.dp() <= 200) {
+                this.x += Math.sign(plr.x - this.x);
+                this.y += Math.sign(plr.y - this.y);
+            }
+            if(this.atkCd.ready) {
+                atk();
+                this.atkCd.consume();
+            }
         } });
         this.use("health", { hp, onHurt: () => {
             this.color = "#8b0b0b";
             setTimeout(() => this.color = c, 125);
         }, onDie: () => scene.rm(this) });
-        this.dmg = dmg;
+        this.atkCd = new Cooldown(cd, false);
+    }
+    dp() {
+        return Vector.dist(this.getPos(), plr.getPos());
     }
 }
-class BasicEnemy extends Enemy {
+class MeleeEnemy extends Enemy {
+    constructor(x: number, y: number, w: number, h: number, c: string, hp: number, dmg: number, range: number, cd: number) {
+        super(x, y, w, h, c, hp, dmg, () => {
+            if(this.dp() <= range) {
+                plr.comp("health").hurt(dmg);
+            }
+        }, cd);
+    }
+}
+class GunEnemy extends Entity {
+    constructor(x: number, y: number, w: number, h: number, c: string, hp: number, dmg: number, getRot: () => number, bspd: number, cd: number, asWell?: (e: Entity) => void) {
+        super(x, y, w, h, c, hp, dmg, () => {
+            const o = bulGenr(this.x, this.y, getRot(), (e) => {
+                if(e == plr) {
+                    e.comp("health").hurt(dmg);
+                    scene.rm(o);
+                    if(asWell) asWell(e);
+                }
+            }, bspd);
+            scene.add(o);
+        }, cd);
+    }
+}
+class BasicMeleeEnemy extends MeleeEnemy {
     constructor(x: number, y: number) {
-        super(x, y, 1, 2, "#ec0303", 5, 0);
+        super(x, y, 1, 2, "#ec0303", 5, 1, 100, 150);
     }
 }
+class BasicGunEnemy extends GunEnemy {
+    constructor(x: number, y: number, roff = 10) {
+        super(x, y, 1, 2, "#ec5c03", 5, 1, () => {
+            const r = scene.rotBtwn(this, plr);
+            return Angle.rad(random(Angle.deg(r) - roff, Angle.deg(r) + roff));
+        }, 3, 150);
+    }
+}
+
 class Exit extends Entity {
     constructor(x: number, y: number, rot: number, then: Vector) {
         super({ x, y, rot, width: 5, height: 10, color: "#fa5700", collide: (e) => {
@@ -54,7 +96,7 @@ function TopExit() { return new Exit(scene.width / 2, 0, Angle.rad(90), new Vect
 function BtmExit() { return new Exit(scene.width / 2, scene.height, Angle.rad(90), new Vector(0, -1)); }
 
 const rooms: Room[] = [
-    { at: new Vector(0, 0), e: [new BasicEnemy(0, 0)], exit: [] }
+    { at: new Vector(0, 0), e: [new BasicMeleeEnemy(0, 0)], exit: [] }
 ];
 function fdRm() {
     return rooms.find(r => r.at.x == pos.x && r.at.y == pos.y);
@@ -63,11 +105,14 @@ function ldRm() {
     const rm = fdRm();
     if(rm) scene.add(...rm.e);
 }
+function bulGenr(x: number, y: number, rot: number, collide: (e: Entity) => void, spd: number) {
+    return new BulletObject({ x, y, rot, height: 6, width: 18, scene, color: "#e2e603", collide, extLeft: 0, extRight: scene.width, extTop: 0, extBtm: scene.height, spd });
+}
 ldRm();
 
 scene.add(plr);
 scene.on("click", () => {
-    const o = new BulletObject({ x: plr.x, y: plr.y, rot: scene.rotToMouse(plr), height: 6, width: 18, scene, color: "#e2e603", collide: (e) => { if(objIs(e, Enemy)) { e.comp("health").hurt(stat.dmg); scene.rm(o); } }, extLeft: 0, extRight: scene.width, extTop: 0, extBtm: scene.height, spd: stat.bspd });
+    const o = bulGenr(plr.x, plr.y scene.rotToMouse(plr), (e) => { if(objIs(e, Enemy)) { e.comp("health").hurt(stat.dmg); scene.rm(o); } }, stat.bspd);
     scene.add(o);
 });
 scene.start(() => {
