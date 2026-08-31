d842678eeebfc25d0ecce41b317bf6842cda7c77
BeanTheAlien <ben.allen.goldstein@gmail.com>
2026-08-31 15:07:19 -0500

    add more comments

--- games/dngrm/index.ts
+++ games/dngrm/index.ts
@@ -134,6 +134,22 @@ const healthOpts = (self: Entity, hp: number, onDie: Function, c1: string, c2: s
  * Coughing Baby
  * He's VERY evil.
  * Wep: Germs
+ * 
+ * Boring Bob
+ * Zzz...
+ * Wep: Boring
+ * 
+ * Marmaduke
+ * Go go gadget amry!
+ * Wep: Army
+ * 
+ * Ocean
+ * Mmmm waves.
+ * Wep: Waves
+ * 
+ * Axel Axton
+ * Arrested 7 times for felony murder charges.
+ * Wep: Car Axle
  */
 const plr = new PlayableCharacter({ strength: 0, width: size, height: size, color: "#29ad05", upd: () => {
     plr.rot = scene.rotToMouse(plr);
