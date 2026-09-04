944c0e1a3f676c52a85c3ffae0d04d3807591050
BeanTheAlien <ben.allen.goldstein@gmail.com>
2026-09-04 13:08:48 +0000

    CRITICAL: COMPILE SV

--- sv.js
+++ sv.js
@@ -3,4 +3,5 @@ const url = "https://beanthealien-server.onrender.com/";
 const net = new AdvancedNetMap(url);
 const adminNet = new AdvancedNetMap(url + "admin/");
 const comNet = new AdvancedNetMap(url + "com/");
+const annNet = new AdvancedNetMap(url + "anns/");
 export { net, adminNet, comNet };
