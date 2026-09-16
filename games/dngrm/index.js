f29054f36734aa20c8888aad53a315c28bfb35a2
BeanTheAlien <ben.allen.goldstein@gmail.com>
2026-09-16 17:33:02 +0000

    fix splice

--- games/dngrm/index.js
+++ games/dngrm/index.js
@@ -873,7 +873,7 @@ function newPchUIs() {
                         stat.mon -= p.ct;
                         p.fx();
                         scene.rmUI(...uis);
-                        pchsUI.splice(pchsUI.indexOf(uis));
+                        pchsUI.splice(pchsUI.indexOf(uis), 1);
                     }
                 } })
         ];
--- games/dngrm/index.ts
+++ games/dngrm/index.ts
@@ -994,7 +994,7 @@ function newPchUIs() {
                     stat.mon -= p.ct;
                     p.fx();
                     scene.rmUI(...uis);
-                    pchsUI.splice(pchsUI.indexOf(uis));
+                    pchsUI.splice(pchsUI.indexOf(uis), 1);
                 }
             } })
         ];
